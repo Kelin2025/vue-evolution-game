c83f6f643148769bc10e2a601208c45a11001036
Anton Kosykh <kelin2025@yandex.ru>
2018-06-03 04:06:24 +0300

    Lobby store module [WIP]

---
+++ src/store/lobby.js
@@ -0,0 +1,71 @@
+import { curry } from "nanoutils";
+
+const STAGES = ["turn", "feed", "extinction"];
+
+const unlessGteThan = curry(
+  (maxVal, orVal, val) => (val >= maxVal ? orVal : val)
+);
+
+const nextOrFirstIdx = curry((list, val) => unlessGteThan(list.length, 0, val));
+
+export const state = {
+  id: null,
+  stage: "turn",
+  cards: [],
+  players: [],
+  currentPlayerIdx: 0
+};
+
+export const mutations = {
+  reset(state) {
+    state.id = null;
+    state.stage = "turn";
+    state.cards = [];
+    state.currentPlayerIdx = 0;
+  },
+
+  setId(state, id) {
+    state.id = id;
+  },
+
+  addCards(state, cards) {
+    state.cards.push(...cards);
+  },
+
+  addPlayers(state, players) {
+    state.players.push(...players);
+  },
+
+  setCurrentPlayerIdx(state, currentPlayerIdx) {
+    state.currentPlayerIdx = currentPlayerIdx;
+  },
+
+  setStage(state, stage) {
+    state.stage = stage;
+  }
+};
+
+export const actions = {
+  start({ commit }, { id, cards, players }) {
+    commit("reset");
+    commit("setId", id);
+    commit("addCards", cards);
+    commit("addPlayers", players);
+  },
+
+  setStage({ commit }, stage) {
+    commit("setStage", stage);
+  },
+
+  nextTurn({ state, commit }) {
+    const nextIdx = nextOrFirstIdx(state.players, state.currentPlayerIdx + 1);
+    commit("setCurrentPlayerIdx", nextIdx);
+  },
+
+  nextStage({ commit }) {
+    commit(
+      "setStage",
+      STAGES[nextOrFirstIdx(STAGES, STAGES.indexOf(state.stage) + 1)]
+    );
+  }
+};
