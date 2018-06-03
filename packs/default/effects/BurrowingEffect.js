ed1fa8c898c0f9cf78072818ec3edb691dd9f15d
Anton Kosykh <kelin2025@yandex.ru>
2018-06-04 01:06:30 +0300

    Removed unnecessary getter

--- packs/default/effects/BurrowingEffect.js
+++ packs/default/effects/BurrowingEffect.js
@@ -1,11 +1,7 @@
 import { Effect } from "~/models/effect";
 
 export class BurrowingEffect extends Effect {
-  get canSave() {
-    return this.creature.isFed;
-  }
-
   onCreatureDefends() {
-    return !!this.canSave;
+    return !!this.creature.isFed;
   }
 }
