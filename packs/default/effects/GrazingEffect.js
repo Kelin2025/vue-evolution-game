9bafe0c6a8b29900ed2ad790c3cb2037e8f16daf
Anton Kosykh <kelin2025@yandex.ru>
2018-06-04 01:13:59 +0300

    Added 2 turns block for Hibernation effect + added isClickable property for Grazing effect

--- packs/default/effects/GrazingEffect.js
+++ packs/default/effects/GrazingEffect.js
@@ -3,6 +3,10 @@ import { Effect } from "~/models/effect";
 export class GrazingEffect extends Effect {
   isBlocked = false;
 
+  get isClickable() {
+    return !this.isBlocked;
+  }
+
   onStageEnd({ stage }) {
     if (stage === "feed") {
       this.isBlocked = false;
--- packs/default/effects/HibernationEffect.js
+++ packs/default/effects/HibernationEffect.js
@@ -1,7 +1,20 @@
 import { Effect } from "~/models/effect";
 
 export class HibernationEffect extends Effect {
+  turnsBlock = 0;
+
+  get isClickable() {
+    return this.turnsBlock === 0;
+  }
+
+  onStageEnd() {
+    if (this.turnsBlock > 0) {
+      this.turnsBlock -= 1;
+    }
+  }
+
   onOwnerClick() {
+    this.turnsBlock = 2;
     this.creature.currentFood = this.creature.requiredFood;
   }
 }
