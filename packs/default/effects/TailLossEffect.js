aee750d0ea8d13e44eaa79da7544a5975b73c660
Anton Kosykh <kelin2025@yandex.ru>
2018-06-03 04:11:19 +0300

    TailLoss effect

---
+++ packs/default/effects/TailLossEffect.js
@@ -0,0 +1,17 @@
+import { Effect } from "~/models/Effect";
+
+export class TailLossEffect extends Effect {
+  // TODO: Implement "Choose effect" global method
+  async onCreatureDefends() {
+    // const effect = await game.chooseEffect({
+    //   creature: this.creature,
+    //   abortable: true
+    // });
+    // if (effect) {
+    //   this.creature.removeEffect(effect);
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  }
+}
