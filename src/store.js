3dcee93353b72704159b3c3bb39b166157b65d58
Anton Kosykh <kelin2025@yandex.ru>
2018-06-04 03:07:14 +0300

    Store auto-required modules

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+// Require vuex modules
+const requireModule = require.context("./store", true, /\.js$/);
+const modules = {};
+requireModule.keys().forEach(m => {
+  const moduleName = m.slice(2, -3);
+  modules[moduleName] = requireModule(m);
+  modules[moduleName].namespaced = true;
+});
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  modules
 });
